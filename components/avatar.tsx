3c79d2421e157e0982e675a4a27a8fa8098fa4cc
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2021-08-23 20:04:00 +0200

    Play with theme margins and paddings making things readable

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,24 +1,34 @@
 import Image from "next/image";
+type AvatarProps = {
+  picture: string;
+};
+
+const Avatar = ({ picture }: AvatarProps) => {
+  return (
+    <div className="h-12 w-12 relative mr-4">
+      <Image
+        src={picture}
+        alt="Picture of the author"
+        layout="fill" // required
+        objectFit="cover" // change to suit your needs
+        className="rounded-full" // just an example
+      />
+    </div>
+  );
+};
+
 type Props = {
   name: string;
   picture: string;
 };
 
-const Avatar = ({ name, picture }: Props) => {
+const AvatarWithAuthor = ({ name, picture }: Props) => {
   return (
     <div className="flex items-center ">
-      <div className="h-12 w-12 relative mr-4">
-        <Image
-          src={picture}
-          alt="Picture of the author"
-          layout="fill" // required
-          objectFit="cover" // change to suit your needs
-          className="rounded-full" // just an example
-        />
-      </div>
-      <div className="text-xl font-bold">{name}</div>
+      <Avatar picture={picture} />
+      <div className="text-xl font-bold">by {name}</div>
     </div>
   );
 };
 
-export default Avatar;
+export default AvatarWithAuthor;
--- components/post-header.tsx
+++ components/post-header.tsx
@@ -1,36 +1,37 @@
-import Avatar from './avatar'
-import DateFormatter from './date-formatter'
-import CoverImage from './cover-image'
-import PostTitle from './post-title'
-import Author from '../types/author'
+import Avatar from "./avatar";
+import DateFormatter from "./date-formatter";
+import CoverImage from "./cover-image";
+import PostTitle from "./post-title";
+import Author from "../types/author";
 
 type Props = {
-  title: string
-  coverImage: string
-  date: string
-  author: Author
-}
+  title: string;
+  coverImage: string;
+  date: string;
+  author: Author;
+};
+
+const Date = ({ date }: { date: string }) => {
+  return (
+    <div className="font-bold flex text-lg mr-4">
+      <DateFormatter dateString={date} />
+    </div>
+  );
+};
 
 const PostHeader = ({ title, coverImage, date, author }: Props) => {
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="hidden md:block md:mb-12">
+      <div className="flex-col items-center md:flex-row mb-12">
+        <Date date={date} />
         <Avatar name={author.name} picture={author.picture} />
       </div>
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} src={coverImage} />
       </div>
-      <div className="max-w-2xl mx-auto">
-        <div className="block md:hidden mb-6">
-          <Avatar name={author.name} picture={author.picture} />
-        </div>
-        <div className="mb-6 text-lg">
-          <DateFormatter dateString={date} />
-        </div>
-      </div>
     </>
-  )
-}
+  );
+};
 
-export default PostHeader
+export default PostHeader;
