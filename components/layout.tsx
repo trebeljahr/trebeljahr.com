7142005ea639f23817514d3f6036836c02213993
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2021-09-06 19:41:41 +0200

    Fix Prism Highlighting bug

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,21 +1,11 @@
 // import Footer from "./footer";
 import Meta from "./meta";
-import { useEffect } from "react";
-import Prism from "prismjs";
-import "prismjs/plugins/toolbar/prism-toolbar";
-import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      Prism.highlightAll();
-    }
-  }, []);
-
   return (
     <div className="main-page">
       <Meta />
--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
+import Prism from "prismjs";
+import "prismjs/plugins/toolbar/prism-toolbar";
+import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
+
 type Props = {
   content: string;
 };
 
 const PostBody = ({ content }: Props) => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      console.log("Running Prism Highlight Function");
+      setTimeout(Prism.highlightAll, 1000);
+    }
+  }, [content]);
+
   return (
     <div className="markdown" dangerouslySetInnerHTML={{ __html: content }} />
   );
