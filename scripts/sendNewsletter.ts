c85c5cadd8ae038bc91b2c58bf33cb5eb3c96af3
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-09-18 20:42:04 +0200

    Add working markdown -> HTML parser

--- scripts/sendNewsletter.ts
+++ scripts/sendNewsletter.ts
@@ -1,64 +1,14 @@
 import Handlebars from "handlebars";
 import { readFile } from "fs/promises";
 // import { read } from "to-vfile";
-import { newsletterListMail } from "../lib/mailgun";
-import remarkParse1 from "remark-parse";
-import remarkHtml1 from "remark-html";
-
+// import { newsletterListMail } from "../lib/mailgun";
 import path from "path";
-import { type Processor } from "unified";
-type Unified = () => Processor;
-
-let unified: Unified | undefined;
-
-async function getUnified(): Promise<Unified> {
-  if (typeof unified !== "undefined") return unified;
-  const mod = await (eval(`import('unified')`) as Promise<
-    typeof import("unified")
-  >);
-  ({ unified } = mod);
-  return unified;
-}
-
-let remarkParse: any | undefined;
-async function getRemarkParse(): Promise<any> {
-  if (typeof remarkParse !== "undefined") return remarkParse;
-  const mod = await (eval(`import('remark-parse')`) as Promise<
-    typeof import("remark-parse")
-  >);
-  remarkParse = mod;
-  return remarkParse;
-}
-
-let remarkHtml: any | undefined;
-async function getRemarkHtml(): Promise<any> {
-  if (typeof remarkHtml !== "undefined") return remarkHtml;
-  const mod: any = await (eval(`import('remark-html')`) as Promise<
-    typeof import("remark-html")
-  >);
-
-  remarkHtml = mod;
-  return remarkHtml;
-}
-
-let read: any | undefined;
-async function getRead(): Promise<any> {
-  if (typeof read !== "undefined") return read;
-  const mod: any = await (eval(`import('to-vfile')`) as Promise<
-    typeof import("to-vfile")
-  >);
-
-  ({ read } = mod);
-  return read;
-}
+import remarkHtml from "remark-html";
+import remarkParse from "remark-parse";
+import { unified } from "unified";
+import { read } from "to-vfile";
 
 async function main() {
-  await getUnified();
-  await getRemarkParse();
-  await getRemarkHtml();
-  await getRead();
-  if (!unified || !remarkParse || !remarkHtml || !read) return;
-
   const emailHandlebarsFile = await readFile(
     path.join(process.cwd(), "..", "email-templates", "newsletter.hbs"),
     "utf-8"
