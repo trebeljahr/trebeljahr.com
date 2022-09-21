b7ca0560ac86715b4a8f4f74d073eaabf0431665
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-09-21 11:44:49 +0200

    Use unified to convert md -> emails!

--- scripts/sendNewsletter.ts
+++ scripts/sendNewsletter.ts
@@ -6,9 +6,11 @@ import Handlebars from "handlebars";
 
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
+import rehypeRewrite from "rehype-rewrite";
 import rehypeUrls from "rehype-urls";
 import rehypePresetMinify from "rehype-preset-minify";
 import rehypeStringify from "rehype-stringify";
+import { Root } from "remark-parse/lib/index.js";
 
 const newsletterNumber = 1;
 const LIVE_HOST = "https://trebeljahr.com";
@@ -27,21 +29,46 @@ async function main() {
   );
 
   function addHost(url: any, node: any) {
-    if (url.path.startsWith("/")) {
+    console.log(url.href.startsWith("/"));
+    if (url.href.startsWith("/")) {
+      console.log(url);
+      console.log(node);
       return LIVE_HOST + url.path;
     }
   }
 
-  function logUrls(url: any, node: any) {
-    console.log(url);
-    console.log(node);
+  function rewriteImages(node: any) {
+    if (node.type == "element" && node.tagName == "img") {
+      node.properties = {
+        ...node.properties,
+        width: "600",
+        height: "",
+        border: "0",
+        style: `
+          width: 100%;
+          max-width: 600px;
+          height: auto;
+          background: #dddddd;
+          font-family: sans-serif;
+          font-size: 15px;
+          line-height: 15px;
+          color: #555555;
+          margin: 10px auto;
+          display: block;
+        `,
+        class: "g-img",
+      };
+    }
   }
 
   const file = await unified()
     .use(remarkParse)
     .use(remarkRehype, { allowDangerousHtml: true })
     .use(rehypeUrls, addHost)
-    .use(rehypeUrls, logUrls)
+    // .use(rehypeUrls, logUrls)
+    .use(rehypeRewrite, {
+      rewrite: rewriteImages,
+    })
     .use(rehypePresetMinify)
     .use(rehypeStringify)
     .process(mdFileRaw);
