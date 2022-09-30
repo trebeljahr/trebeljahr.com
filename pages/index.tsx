dc94b16859e87b2b452e4c25c961f39364bf7b99
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-09-30 23:01:10 +0200

    Add margins also to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,56 +8,65 @@ import { NewsletterForm } from "../components/newsletter-signup";
 const Index = () => {
   return (
     <Layout pageTitle="Home">
-      <h2>Hi there 👋🏻</h2>
-      <p>
-        I am Rico. A programmer, traveler, photographer, writer and fellow
-        human. This is my personal website.
-      </p>
-      <p>
-        Here is where I collect thoughts, booknotes, quotes, ideas, traveling
-        stories, photography, side projects and links to my favorite places in
-        the internet. For now I still have an old - second site - over at{" "}
-        <ExternalLink href="https://ricotrebeljahr.com">
-          ricotrebeljahr.com
-        </ExternalLink>{" "}
-        but the content will eventually also move over here.
-      </p>
-      <TrySomeOfThese />
-      <p>
-        If you want to know what I am doing currently, head over to{" "}
-        <Link as={`/now`} href="/now">
-          <a>/now</a>
-        </Link>
-        .
-      </p>
+      <article>
+        <section className="main-section">
+          <h2>Hi there 👋🏻</h2>
+          <p>
+            I am Rico. A programmer, traveler, photographer, writer and fellow
+            human. This is my personal website.
+          </p>
+          <p>
+            Here is where I collect thoughts, booknotes, quotes, ideas,
+            traveling stories, photography, side projects and links to my
+            favorite places in the internet. For now I still have an old -
+            second site - over at{" "}
+            <ExternalLink href="https://ricotrebeljahr.com">
+              ricotrebeljahr.com
+            </ExternalLink>{" "}
+            but the content will eventually also move over here.
+          </p>
+          <TrySomeOfThese />
+          <p>
+            If you want to know what I am doing currently, head over to{" "}
+            <Link as={`/now`} href="/now">
+              <a>/now</a>
+            </Link>
+            .
+          </p>
 
-      <h3>Webpages</h3>
-      <p>
-        You can also find me on other places around the internet, like{" "}
-        <ExternalLink href="https://www.instagram.com/ricotrebeljahr/">
-          Instagram
-        </ExternalLink>
-        ,{" "}
-        <ExternalLink href="https://github.com/trebeljahr">Github</ExternalLink>
-        ,{" "}
-        <ExternalLink href="https://www.linkedin.com/in/trebeljahr">
-          LinkedIn
-        </ExternalLink>{" "}
-        or{" "}
-        <ExternalLink href="https://twitter.com/ricotrebeljahr">
-          Twitter
-        </ExternalLink>
-        .
-      </p>
-      <p>
-        I also used to write a travel blog with a friend of mine, which you can
-        still find at{" "}
-        <ExternalLink href="https://photodyssee.com">
-          photodyssee.com
-        </ExternalLink>
-        .
-      </p>
-      <NewsletterForm />
+          <h3>Webpages</h3>
+          <p>
+            You can also find me on other places around the internet, like{" "}
+            <ExternalLink href="https://www.instagram.com/ricotrebeljahr/">
+              Instagram
+            </ExternalLink>
+            ,{" "}
+            <ExternalLink href="https://github.com/trebeljahr">
+              Github
+            </ExternalLink>
+            ,{" "}
+            <ExternalLink href="https://www.linkedin.com/in/trebeljahr">
+              LinkedIn
+            </ExternalLink>{" "}
+            or{" "}
+            <ExternalLink href="https://twitter.com/ricotrebeljahr">
+              Twitter
+            </ExternalLink>
+            .
+          </p>
+          <p>
+            I also used to write a travel blog with a friend of mine, which you
+            can still find at{" "}
+            <ExternalLink href="https://photodyssee.com">
+              photodyssee.com
+            </ExternalLink>
+            .
+          </p>
+        </section>
+      </article>
+      <section className="main-section">
+        <NewsletterForm />
+      </section>
     </Layout>
   );
 };
