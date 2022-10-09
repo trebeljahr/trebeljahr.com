86563ed656b30a8789ffd8aa1271811fa5bfe86a
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-10-09 09:36:22 +0200

    First twitter automation

--- scripts/tweet.ts
+++ scripts/tweet.ts
@@ -1,10 +1,16 @@
+import { config } from "dotenv";
+config();
 import { TwitterApi } from "twitter-api-v2";
 
-const twitterClient = new TwitterApi({
+const twitter = new TwitterApi({
   appKey: process.env.TWITTER_APP_KEY,
   appSecret: process.env.TWITTER_APP_SECRET,
   accessToken: process.env.TWITTER_ACCESS_TOKEN,
   accessSecret: process.env.TWITTER_ACCESS_SECRET,
-});
+}).readWrite;
 
-await twitterClient.readWrite.v1.tweet("this is a test");
+const media_ids = await twitter.v1.uploadMedia("./twitter-recursion.png");
+
+await twitter.v1.tweet("This tweet is this tweet?", {
+  media_ids,
+});
