b8a321aa4f1e82db12356ac6330135fa4094922b
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-11-11 10:24:13 +0100

    Replace Chart Js by own driver code

--- src/components/neural-net/NeuronDemo.tsx
+++ src/components/neural-net/NeuronDemo.tsx
@@ -1,16 +1,9 @@
-import { Scatter } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Legend,
-} from "chart.js";
 import { Point } from "../../lib/math/datasets/utils";
 import { createCircleData } from "../../lib/math/datasets/circle";
 import { createGaussData } from "../../lib/math/datasets/gauss";
 import { createSpiralData } from "../../lib/math/datasets/spiral";
 import { createXorData } from "../../lib/math/datasets/xor";
+import { Plot } from "./Plot";
 
 function mapToDataSets(datasets: [Point[], Point[]]) {
   return {
@@ -41,22 +34,10 @@ const ScatterPlot = () => {
   };
   return (
     <>
-      <Scatter
-        options={scatterPlotOptions}
-        data={mapToDataSets(createCircleData(1000))}
-      />
-      <Scatter
-        options={scatterPlotOptions}
-        data={mapToDataSets(createGaussData(1000))}
-      />
-      <Scatter
-        options={scatterPlotOptions}
-        data={mapToDataSets(createSpiralData(1000))}
-      />
-      <Scatter
-        options={scatterPlotOptions}
-        data={mapToDataSets(createXorData(1000))}
-      />
+      <Plot data={createCircleData(1000)} />
+      <Plot data={createGaussData(1000)} />
+      <Plot data={createXorData(1000)} />
+      <Plot data={createSpiralData(1000)} />
     </>
   );
 };
---
+++ src/components/neural-net/Plot.tsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import SimpleReactCanvasComponent from "simple-react-canvas-component";
+import { useActualSize } from "../../hooks/useWindowSize";
+import { createCircleData } from "../../lib/math/datasets/circle";
+import { Point } from "../../lib/math/datasets/utils";
+import {
+  circle,
+  drawBackground,
+  drawCoordinateSystem,
+} from "../../lib/math/drawHelpers";
+import { Vec2 } from "../../lib/math/Vector";
+
+function mapToDataSets(datasets: [Vec2[], Vec2[]]) {
+  return [
+    {
+      data: datasets[0],
+      color: "rgba(255, 99, 132, 1)",
+    },
+    {
+      data: datasets[1],
+      color: "rgba(50, 99, 255, 1)",
+    },
+  ];
+}
+
+function plotDatasets(ctx: CanvasRenderingContext2D, datasets: Dataset[]) {
+  for (let dataset of datasets) {
+    plotDataset(ctx, dataset);
+  }
+}
+
+function plotDataset(ctx: CanvasRenderingContext2D, dataset: Dataset) {
+  ctx.fillStyle = dataset.color;
+  for (let point of dataset.data) {
+    console.log(point);
+    circle(ctx, new Vec2(point.x, point.y), 5);
+  }
+}
+
+type Dataset = {
+  data: Vec2[];
+  color: string;
+};
+
+type Props = {
+  data: [Point[], Point[]];
+};
+
+export function Plot({ data }: Props) {
+  const { width, height } = useActualSize();
+
+  const [cnv, setCnv] = useState<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    if (!cnv) return;
+    const ctx = cnv.getContext("2d");
+    if (!ctx || !width || !height) return;
+
+    const origin = new Vec2(width / 2, height / 2);
+    const scaleFactor = Math.floor(Math.max(width, height) / 20);
+
+    const vecData = data.map((points) =>
+      points.map(({ x, y }) => new Vec2(x, y).scale(scaleFactor).add(origin))
+    ) as [Vec2[], Vec2[]];
+
+    const drawFn = () => {
+      drawBackground(ctx);
+      drawCoordinateSystem(ctx, scaleFactor);
+      plotDatasets(ctx, mapToDataSets(vecData));
+    };
+
+    drawFn();
+  }, [cnv, width, height, data]);
+  return (
+    <SimpleReactCanvasComponent setCnv={setCnv} width={width} height={height} />
+  );
+}
