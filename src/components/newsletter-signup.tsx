b5f91a4f14af40679996c7e6587c0d6398d7ceab
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-11-20 09:20:01 +0100

    Add more things to newsletter

--- src/components/newsletter-signup.tsx
+++ src/components/newsletter-signup.tsx
@@ -76,8 +76,9 @@ export const NewsletterForm = () => {
         <>
           <h2>Subscribe to my newsletter</h2>
           <p>
-            Twice a month. 1 Booknote. 1 Photo. 1 Post. Awesomeness bundled
-            together in one heck of a Newsletter. No spam. No noise.{" "}
+            Twice a month. A quote, a photo, a booknote and some Links.
+            Awesomeness bundled together in one heck of a Newsletter. No spam.
+            No noise.{" "}
           </p>
 
           {error && <p className="error">{error}</p>}
