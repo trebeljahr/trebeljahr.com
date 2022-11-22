b9c26152ce660bc84943e8e790b117fe8aa57fd6
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-11-22 23:29:15 +0100

    Add working 3d demo!

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,3 +1,38 @@
 import { MagnitudeDemo } from "./collision-detection/MagnitudeDemo";
 
 export const SomeDemo = () => <MagnitudeDemo />;
+import * as THREE from "three";
+import { useRef, useState } from "react";
+import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
+
+function Box(props: ThreeElements["mesh"]) {
+  const ref = useRef<THREE.Mesh>(null!);
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  useFrame(() => (ref.current.rotation.x += 0.01));
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
+export const ThreeFiberDemo = () => {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
+};
