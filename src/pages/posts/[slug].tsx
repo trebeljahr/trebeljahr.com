8a9593ad64f501321db08dc4b953e3cdadd35f2c
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-11-22 16:24:24 +0100

    Fix more contentlayer stuff

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,90 +1,90 @@
-import { useRouter } from "next/router";
-import ErrorPage from "next/error";
-import PostBody from "../../components/post-body";
+import { useMDXComponent } from "next-contentlayer/hooks";
+import { allPosts } from "contentlayer/generated";
+import type { Post as PostType } from "contentlayer/generated";
+
 import PostHeader from "../../components/post-header";
 import Layout from "../../components/layout";
-import { getPostBySlug, getAllPosts, getAllMarkdownPosts } from "../../lib/api";
-import { ReadMore } from "../../components/more-stories";
-import { ToTopButton } from "../../components/ToTopButton";
-import { Post as PostType } from "../../@types/post";
 import { NewsletterForm } from "../../components/newsletter-signup";
+import { ToTopButton } from "../../components/ToTopButton";
+import { ReadMore } from "../../components/more-stories";
 import { getRandom } from "src/lib/math/getRandom";
+import { NormalDemo } from "../../components/collision-detection/NormalDemo";
+import { DotProductDemo } from "../../components/collision-detection/DotProductDemo";
+import { UnitVectorDemo } from "../../components/collision-detection/UnitVectorDemo";
+import { MagnitudeDemo } from "../../components/collision-detection/MagnitudeDemo";
+import { RotationDemo } from "../../components/collision-detection/RotationDemo";
+import { PointAndVectorDemo } from "../../components/collision-detection/PointAndVectorDemo";
+import { SinAndCosineDemo } from "../../components/collision-detection/SinAndCosineDemo";
 
 type Props = {
+  children: React.ReactNode;
+  morePosts: { slug: string; title: string }[];
   post: PostType;
-  morePosts: PostType[];
 };
 
-const Post = ({ post, morePosts }: Props) => {
+export const BlogLayout = ({
+  children,
+  morePosts,
+  post: { excerpt, title, subtitle, date, author },
+}: Props) => (
+  <Layout description={excerpt} title={title + " – " + subtitle}>
+    <article className="post-body">
+      <section className="main-section main-text">
+        <PostHeader
+          subtitle={subtitle}
+          title={title}
+          date={date}
+          author={author}
+        />
+        {children}
+      </section>
+      <section className="main-section">
+        {morePosts && <ReadMore posts={morePosts} />}
+        <NewsletterForm />
+        <ToTopButton />
+      </section>
+    </article>
+  </Layout>
+);
+
+type BlogProps = {
+  post: PostType;
+  morePosts: { slug: string; title: string }[];
+};
+
+export default function PostComponent({ post, morePosts }: BlogProps) {
+  const Component = useMDXComponent(post.body.code);
+
   return (
-    <Layout
-      description={post.excerpt}
-      title={post.title + " – " + post.subtitle}
-    >
-      <article className="post-body">
-        <section className="main-section">
-          <PostHeader
-            subtitle={post.subtitle}
-            title={post.title}
-            date={post.date}
-            author={post.author}
-          />
-          <PostBody content={post.content} />
-        </section>
-        <section className="main-section">
-          {morePosts && <ReadMore posts={morePosts} />}
-          <NewsletterForm />
-          <ToTopButton />
-        </section>
-      </article>
-    </Layout>
+    <BlogLayout post={post} morePosts={morePosts}>
+      <Component
+        components={{
+          DotProductDemo,
+          MagnitudeDemo,
+          NormalDemo,
+          PointAndVectorDemo,
+          UnitVectorDemo,
+          RotationDemo,
+          SinAndCosineDemo,
+        }}
+      />
+    </BlogLayout>
   );
-};
-
-export default Post;
-
-type Params = {
-  params: {
-    slug: string;
-  };
-};
-
-export async function getStaticProps({ params }: Params) {
-  const post = await getPostBySlug(params.slug + ".md", [
-    "title",
-    "subtitle",
-    "date",
-    "slug",
-    "author",
-    "excerpt",
-    "content",
-    "cover",
-  ]);
-  const bySlug = (otherPost: any) => otherPost.slug !== post.slug;
-  const otherPosts = (await getAllPosts(["title", "slug", "excerpt"])).filter(
-    bySlug
-  );
-  const morePosts = getRandom(otherPosts, 3);
-
-  return {
-    props: {
-      post,
-      morePosts,
-    },
-  };
 }
 
 export async function getStaticPaths() {
-  const posts = await getAllMarkdownPosts(["slug"]);
-
   return {
-    paths: posts.map((post) => {
-      return {
-        params: {
-          slug: post.slug,
-        },
-      };
-    }),
+    paths: allPosts.map((post: PostType) => ({ params: { slug: post.slug } })),
     fallback: false,
   };
+}
+
+export async function getStaticProps({ params }: { params: { slug: string } }) {
+  const post = allPosts.find((post: PostType) => post.slug === params.slug);
+  const otherPosts = allPosts
+    .filter((post) => post.slug !== params.slug)
+    .map(({ slug, title }) => ({ slug, title }));
+  const morePosts = getRandom(otherPosts, 3);
+
+  return { props: { post, morePosts } };
 }
