3be74dc3965402cdc593ae99dc8892ff0f071589
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-11-26 20:20:24 +0100

    Add tagging experiment code

--- src/pages/experiment.tsx
+++ src/pages/experiment.tsx
@@ -1,24 +1,91 @@
 import Layout from "../components/layout";
 import { ToTopButton } from "../components/ToTopButton";
 import { NewsletterForm } from "../components/newsletter-signup";
-import { allDocuments } from "contentlayer/generated";
+import { allDocuments, DocumentTypes } from "contentlayer/generated";
+import Link from "next/link";
+
+const mainCategories = [
+  "philosophy",
+  "psychology",
+  "neuroscience",
+  "biochemistry",
+  "cells",
+  "physics",
+  "engineering",
+  "personal development",
+  "music",
+  "ai",
+  "programming",
+  "hacking",
+  "finances",
+  "mathematics",
+  "traveling",
+  "design",
+  "politics",
+  "problems",
+  "art",
+];
+type LinksOnTag<T> = { tag: string; links: T[] };
+
+type LinksInfo = LinksOnTag<Pick<DocumentTypes, "title" | "slug" | "type">>;
 
 type Props = {
-  tags: string[];
+  tags: LinksInfo[];
+  categories: LinksInfo[];
 };
 
-const Newsletters = ({ tags }: Props) => {
+const RenderTags = ({ tags }: { tags: LinksInfo[] }) => {
+  return (
+    <div className="tag-filter-container">
+      {tags.map(({ tag, links }) => {
+        return (
+          <Link
+            key={tag}
+            href={"#" + tag}
+            as={"#" + tag}
+            className="tag-filter clickable"
+          >
+            {tag} ({links.length})
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+const ShowTags = ({ tags, categories }: Props) => {
+  console.log(tags);
+  console.log(categories);
   return (
     <Layout
       title="Experiment"
       description="An Experimental Overview over All Pages"
     >
       <article>
-        <section className="main-section">
-          <h1>Tags:</h1>
-          {/* {tags.map((tag) => {
-            <p key={tag}>{tag}</p>;
-          })} */}
+        <section className="main-section main-text">
+          <h2>Categories:</h2>
+          <RenderTags tags={categories} />
+
+          <h2>Tags:</h2>
+          <RenderTags tags={tags} />
+
+          {tags.map(({ tag, links }) => {
+            return (
+              <div key={tag}>
+                <h2 id={tag}>{tag}</h2>
+                <ul>
+                  {links.map(({ slug, title, type }) => {
+                    return (
+                      <li key={slug}>
+                        <Link href={slug} as={slug}>
+                          {title} ({type})
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            );
+          })}
         </section>
         <section className="main-section">
           <NewsletterForm />
@@ -29,7 +96,7 @@ const Newsletters = ({ tags }: Props) => {
   );
 };
 
-export default Newsletters;
+export default ShowTags;
 
 export function getStaticProps() {
   const allTags = allDocuments.flatMap(({ tags }) => tags);
@@ -37,7 +104,34 @@ export function getStaticProps() {
   const dedupedTags = [...new Set(allTags)];
   console.log(dedupedTags);
 
+  const tags = dedupedTags.map((tag) => {
+    return {
+      tag,
+      links: allDocuments
+        .filter(({ tags }) => {
+          return tags.includes(tag);
+        })
+        .map(({ slug, type, title }) => ({ slug, type, title })),
+    };
+  });
+
+  const categories = mainCategories.map((tag) => {
+    return {
+      tag,
+      links: allDocuments
+        .filter(({ tags }) => {
+          return tags.includes(tag);
+        })
+        .map(({ slug, type, title }) => ({ slug, type, title })),
+    };
+  });
+
+  console.log(categories);
+
   return {
-    props: {},
+    props: {
+      tags,
+      categories,
+    },
   };
 }
