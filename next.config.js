a971fdb5baa2daa8a1e8ae703d606715894eb2f6
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2023-03-07 10:49:05 +0100

    Add dependencies for 3D stuff

--- next.config.js
+++ next.config.js
@@ -9,4 +9,76 @@ const nextConfig = {
   },
 };
 
-module.exports = withContentlayer(nextConfig);
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
+const KEYS_TO_OMIT = [
+  "webpackDevMiddleware",
+  "configOrigin",
+  "target",
+  "analyticsId",
+  "webpack5",
+  "amp",
+  "assetPrefix",
+];
+
+module.exports = (_phase, { defaultConfig }) => {
+  const plugins = [[withPWA], [withBundleAnalyzer, {}]];
+
+  const configWith3D = {
+    experimental: {},
+    images: {},
+    reactStrictMode: true,
+    webpack(config, { isServer }) {
+      config.module.rules.push({
+        test: /\.(ogg|mp3|wav|mpe?g)$/i,
+        exclude: config.exclude,
+        use: [
+          {
+            loader: require.resolve("url-loader"),
+            options: {
+              limit: config.inlineImageLimit,
+              fallback: require.resolve("file-loader"),
+              publicPath: `${config.assetPrefix}/_next/static/images/`,
+              outputPath: `${isServer ? "../" : ""}static/images/`,
+              name: "[name]-[hash].[ext]",
+              esModule: config.esModule || false,
+            },
+          },
+        ],
+      });
+
+      config.module.rules.push({
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: ["raw-loader", "glslify-loader"],
+      });
+
+      return config;
+    },
+  };
+
+  const wConfig = plugins.reduce(
+    (acc, [plugin, config]) => plugin({ ...acc, ...config }),
+    {
+      ...defaultConfig,
+      ...nextConfig,
+      ...configWith3D,
+    }
+  );
+
+  const finalConfig = {};
+  Object.keys(wConfig).forEach((key) => {
+    if (!KEYS_TO_OMIT.includes(key)) {
+      finalConfig[key] = wConfig[key];
+    }
+  });
+
+  return withContentlayer(finalConfig);
+};
---
+++ src/components/canvas/Scene.tsx
@@ -0,0 +1,25 @@
+import { Canvas } from "@react-three/fiber";
+import { KeyboardControls, Preload } from "@react-three/drei";
+
+export default function Scene({ children, ...props }) {
+  return (
+    <KeyboardControls
+      map={[
+        { name: "forward", keys: ["ArrowUp", "w", "W"] },
+        { name: "backward", keys: ["ArrowDown", "s", "S"] },
+        { name: "left", keys: ["ArrowLeft", "a", "A"] },
+        { name: "right", keys: ["ArrowRight", "d", "D"] },
+        { name: "jump", keys: ["Space"] },
+        { name: "descend", keys: ["c", "C", "Shift"] },
+        { name: "attack", keys: ["F", "f"] },
+      ]}
+    >
+      <Canvas {...props}>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        {children}
+        <Preload all />
+      </Canvas>
+    </KeyboardControls>
+  );
+}
