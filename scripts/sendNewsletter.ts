c19e755759221bd550f52c5a851de7579815dd3d
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2023-03-19 21:21:15 +0100

    Add first draft

--- scripts/sendNewsletter.ts
+++ scripts/sendNewsletter.ts
@@ -12,7 +12,7 @@ import rehypeUrls from "rehype-urls";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 
-const newsletterNumber = 12;
+const newsletterNumber = 13;
 
 const LIVE_HOST = "https://trebeljahr.com";
 
