047b058c164ba8be28224d0ed46e888a92895edb
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2023-03-21 14:38:30 +0100

    Add first draft for twitter automation

---
+++ src/lib/tweet.ts
@@ -0,0 +1,61 @@
+import { TwitterApi } from "twitter-api-v2";
+import { readFile, writeFile } from "fs/promises";
+import path from "path";
+
+type Quote = {
+  content: string;
+  author: string;
+  picked: boolean;
+  tags: string[];
+};
+
+const options = {
+  appKey: process.env.TWITTER_APP_KEY,
+  appSecret: process.env.TWITTER_APP_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_SECRET,
+} as TwitterApiTokens;
+
+console.log({ options });
+
+interface TwitterApiTokens {
+  appKey: string;
+  appSecret: string;
+  accessToken?: string;
+  accessSecret?: string;
+}
+
+const twitter = new TwitterApi(options).readWrite;
+
+const quotesFilePath = path.join(process.cwd(), "../content/pages/quotes.json");
+
+export async function tweetRandomQuote() {
+  const quotes: Quote[] = JSON.parse(await readFile(quotesFilePath, "utf-8"));
+
+  const unpickedQuotes = quotes.filter((quote) => !quote.picked);
+  if (unpickedQuotes.length === 0) {
+    console.log("No more quotes to tweet!");
+    return;
+  }
+
+  const randomIndex = Math.floor(Math.random() * unpickedQuotes.length);
+  const quote = unpickedQuotes[randomIndex];
+
+  try {
+    const tweetContent = `"${quote.content}" - ${quote.author}`;
+    const tweet = await twitter.v1.tweet(tweetContent);
+    console.log("Tweet sent:", tweet.text);
+
+    const originalQuote = quotes.find(
+      (q: Quote) => q.content === quote.content
+    );
+
+    if (!originalQuote) throw new Error("Quote not found");
+
+    originalQuote.picked = true;
+
+    await writeFile(quotesFilePath, JSON.stringify(quotes, null, 2));
+  } catch (error) {
+    console.error("Error sending tweet:", error);
+  }
+}
