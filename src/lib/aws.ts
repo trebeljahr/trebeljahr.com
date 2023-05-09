527cc8d351dd47f178cad4cc550d237a230defd1
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2023-05-09 16:50:51 +0200

    Add subfolders to Amazon listing

---
+++ src/lib/aws.ts
@@ -0,0 +1,107 @@
+import { ListObjectsV2Command, S3, S3Client } from "@aws-sdk/client-s3";
+
+export const getImageSources = ({
+  imageFileNames,
+}: {
+  imageFileNames: string[];
+}) => {
+  const domain = process.env.NEXT_PUBLIC_STATIC_FILE_URL;
+  if (!domain) {
+    throw new Error("No static file URL provided");
+  }
+
+  return imageFileNames.map((fileName) => ({
+    src: `https://${domain}/${fileName}`,
+  }));
+};
+
+type Options = {
+  prefix?: string;
+};
+
+export const getS3ImageData = async ({ prefix = "" }: Options = {}) => {
+  const accessKeyId = process.env.LOCAL_AWS_ACCESS_KEY_ID;
+  const secretAccessKey = process.env.LOCAL_AWS_SECRET_ACCESS_KEY;
+  const awsRegion = process.env.LOCAL_AWS_REGION;
+  const bucketName = process.env.LOCAL_AWS_BUCKET_NAME;
+
+  if (!accessKeyId || !secretAccessKey || !bucketName || !awsRegion) {
+    throw new Error("No AWS credentials provided");
+  }
+
+  return await getDataFromS3({
+    accessKeyId,
+    secretAccessKey,
+    awsRegion,
+    bucketName,
+    prefix,
+  });
+};
+
+export async function getS3Folders(): Promise<string[]> {
+  const accessKeyId = process.env.LOCAL_AWS_ACCESS_KEY_ID;
+  const secretAccessKey = process.env.LOCAL_AWS_SECRET_ACCESS_KEY;
+  const awsRegion = process.env.LOCAL_AWS_REGION;
+  const bucketName = process.env.LOCAL_AWS_BUCKET_NAME;
+
+  if (!accessKeyId || !secretAccessKey || !bucketName || !awsRegion) {
+    throw new Error("No AWS credentials provided");
+  }
+
+  const s3Client = new S3Client({
+    region: awsRegion,
+    credentials: { accessKeyId, secretAccessKey },
+  });
+
+  const data = await s3Client.send(
+    new ListObjectsV2Command({
+      Bucket: bucketName,
+      Delimiter: "/",
+    })
+  );
+
+  const folders =
+    data.CommonPrefixes?.map((prefix) => prefix.Prefix?.split("/")[0] || "") ||
+    [];
+
+  return folders.sort();
+}
+
+export const getDataFromS3 = async ({
+  accessKeyId,
+  secretAccessKey,
+  awsRegion,
+  bucketName,
+  prefix = "",
+}: {
+  accessKeyId: string;
+  secretAccessKey: string;
+  awsRegion: string;
+  bucketName: string;
+  prefix?: string;
+}) => {
+  const s3Client = new S3Client({
+    region: awsRegion,
+    credentials: { accessKeyId, secretAccessKey },
+  });
+
+  const data = await s3Client.send(
+    new ListObjectsV2Command({
+      Bucket: bucketName,
+      Prefix: `${prefix}/`,
+    })
+  );
+
+  if (data.KeyCount && data.MaxKeys && data.KeyCount >= data.MaxKeys) {
+    throw new Error("Can't display all objects in the bucket");
+  }
+
+  console.log(data);
+  console.log(data.Contents);
+
+  return (
+    data.Contents?.filter(({ Key }) => Key !== `${prefix}/`).map(
+      (file) => file.Key as string
+    ) ?? []
+  );
+};
--- src/pages/photography.tsx
+++ src/pages/photography.tsx
@@ -1,12 +1,9 @@
+import Link from "next/link";
 import Layout from "../components/layout";
-import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
 import Image from "next/image";
+import { getS3Folders, getS3ImageData } from "src/lib/aws";
 
-export default function Photography({
-  imageFileNames,
-}: {
-  imageFileNames: string[];
-}) {
+export default function Photography({ tripNames }: { tripNames: string[] }) {
   return (
     <Layout
       title="Photography"
@@ -14,83 +11,19 @@ export default function Photography({
       url="photography"
     >
       <h1>Photography</h1>
-      {getImageSources({ imageFileNames }).map(({ src }, index) => {
+      {tripNames.map((tripName) => {
         return (
-          <Image
-            key={index}
-            src={src}
-            width="6"
-            height="4"
-            alt={src}
-            sizes="100vw"
-            style={{
-              width: "100%",
-              height: "auto",
-              objectFit: "cover",
-            }}
-          />
+          <Link href={`/photography/${tripName}`} key={tripName}>
+            {tripName}
+          </Link>
         );
       })}
     </Layout>
   );
 }
 
-export const getStaticProps = async () => {
-  const accessKeyId = process.env.LOCAL_AWS_ACCESS_KEY_ID;
-  const secretAccessKey = process.env.LOCAL_AWS_SECRET_ACCESS_KEY;
-  const awsRegion = process.env.LOCAL_AWS_REGION;
-  const bucketName = process.env.LOCAL_AWS_BUCKET_NAME;
-
-  if (!accessKeyId || !secretAccessKey || !bucketName || !awsRegion) {
-    throw new Error("No AWS credentials provided");
-  }
-
-  const imageFileNames = await getImagesFromS3({
-    accessKeyId,
-    secretAccessKey,
-    awsRegion,
-    bucketName,
-  });
-
-  return { props: { imageFileNames } };
-};
-
-const getImageSources = ({ imageFileNames }: { imageFileNames: string[] }) => {
-  const domain = process.env.NEXT_PUBLIC_STATIC_FILE_URL;
-  if (!domain) {
-    throw new Error("No static file URL provided");
-  }
-
-  return imageFileNames.map((fileName) => ({
-    src: `https://${domain}/${fileName}`,
-  }));
-};
-
-const getImagesFromS3 = async ({
-  accessKeyId,
-  secretAccessKey,
-  awsRegion,
-  bucketName,
-}: {
-  accessKeyId: string;
-  secretAccessKey: string;
-  awsRegion: string;
-  bucketName: string;
-}) => {
-  const s3Client = new S3Client({
-    region: awsRegion,
-    credentials: { accessKeyId, secretAccessKey },
-  });
-
-  const data = await s3Client.send(
-    new ListObjectsV2Command({
-      Bucket: bucketName,
-    })
-  );
-
-  if (data.KeyCount && data.MaxKeys && data.KeyCount >= data.MaxKeys) {
-    throw new Error("Can't display all objects in the bucket");
-  }
-
-  return data.Contents?.map((file) => file.Key as string) ?? [];
-};
+export async function getStaticProps() {
+  const tripNames = await getS3Folders();
+  console.log(tripNames);
+  return { props: { tripNames } };
+}
