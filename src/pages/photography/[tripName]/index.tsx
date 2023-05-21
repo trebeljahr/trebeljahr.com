d5a39be8bc2b581ff421f9b71402e9a6f3663dff
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2023-05-21 11:09:31 +0200

    Extract custom Renderer

--- src/pages/photography/[tripName]/index.tsx
+++ src/pages/photography/[tripName]/index.tsx
@@ -27,99 +27,13 @@ import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
 
 import "yet-another-react-lightbox/styles.css";
+import {
+  NextJsImage,
+  NextThumbnailRenderer,
+  SlideImageWithNext,
+} from "src/components/image-gallery/customRenderers";
 // import useLightbox from "src/hooks/useLightbox";
 
-export function SlideImageWithNext({ slide, rect }: RenderSlideProps) {
-  const { imageFit } = useLightboxProps().carousel;
-  // const { openLightbox, renderLightbox } = useLightbox();
-
-  const cover = isImageSlide(slide) && isImageFitCover(slide, imageFit);
-
-  if (!slide.height || !slide.width) return null;
-
-  const width = !cover
-    ? Math.round(
-        Math.min(rect.width, (rect.height / slide.height) * slide.width)
-      )
-    : rect.width;
-
-  const height = !cover
-    ? Math.round(
-        Math.min(rect.height, (rect.width / slide.width) * slide.height)
-      )
-    : rect.height;
-
-  return (
-    <div style={{ position: "relative", width, height }}>
-      <Image
-        fill
-        alt={slide.alt || "this image doesn't have an alt text, sorry"}
-        src={slide.src}
-        loading="eager"
-        // placeholder="blur"
-        draggable={false}
-        style={{ objectFit: cover ? "cover" : "contain" }}
-        sizes={`${Math.ceil((width / window.innerWidth) * 100)}vw`}
-      />
-    </div>
-  );
-}
-
-export function NextJsImage({
-  photo,
-  imageProps: { alt, title, sizes, className, onClick },
-  wrapperStyle,
-}: RenderPhotoProps) {
-  console.log(photo);
-
-  return (
-    <div style={{ ...wrapperStyle, position: "relative" }}>
-      <Image
-        src={photo}
-        fill
-        // width={photo.width}
-        // height={photo.height}
-        placeholder={"blurDataURL" in photo ? "blur" : undefined}
-        {...{ alt, title, className, sizes, onClick }}
-      />
-    </div>
-  );
-}
-
-function NextThumbnailRenderer({
-  slide,
-  rect,
-  render,
-}: {
-  slide: Slide;
-  rect: ContainerRect;
-  render: Render;
-}): ReactNode {
-  console.log({ render });
-  console.log(rect);
-  console.log(slide);
-
-  return (
-    <div
-      style={{
-        position: "relative",
-        bottom: 0,
-        width: rect.width,
-        height: rect.height,
-      }}
-    >
-      <Image
-        fill
-        alt={slide.alt || "this image doesn't have an alt text, sorry"}
-        src={slide.src}
-        loading="eager"
-        draggable={false}
-        style={{ objectFit: "cover" }}
-        sizes={`${Math.ceil((rect.width / window.innerWidth) * 100)}vw`}
-      />
-    </div>
-  );
-}
 export default function ImageGallery({
   images,
   tripName,
@@ -266,5 +180,5 @@ export async function getStaticProps({ params }: StaticProps) {
 
   console.log({ images });
 
-  return { props: { images, tripName: params.tripName } };
+  return { props: { images: images.slice(0, 10), tripName: params.tripName } };
 }
