4145a7c02bea92ae305e579f0f3f8fd021d4627b
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2023-05-24 23:54:32 +0200

    Update static page generation timeout

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
     16, 32, 48, 64, 96, 128, 256, 384, 640, 750, 828, 1080, 1200, 1920, 2048,
     3840,
   ],
+  staticPageGenerationTimeout: 600,
   experimental: {
     esmExternals: false,
   },
