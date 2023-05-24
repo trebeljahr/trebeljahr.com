9fd1de09f6a40e2c003e1d3ac25ddee4d89ebeae
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2023-05-25 00:21:40 +0200

    Delete image sizes option from config again

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
   images: {
     domains: [process.env.NEXT_PUBLIC_STATIC_FILE_URL],
   },
-  imageSizes: [
-    16, 32, 48, 64, 96, 128, 256, 384, 640, 750, 828, 1080, 1200, 1920, 2048,
-    3840,
-  ],
   staticPageGenerationTimeout: 600,
   experimental: {
     esmExternals: false,
