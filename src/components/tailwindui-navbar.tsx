3d47a802bace7d8ea5f7aecd2023a675eaffd61e
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2023-07-17 06:48:40 +0200

    Improve Navbar

--- src/components/tailwindui-navbar.tsx
+++ src/components/tailwindui-navbar.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 
 const navigation = ["posts", "newsletters", "photography"];
 const resources = ["needlestack", "booknotes", "podcastnotes", "quotes"];
-const aboutMe = ["principles", "now"];
+const about = ["principles", "now"];
 
-function classNames(...classes: string[]) {
+function combine(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
@@ -18,7 +18,7 @@ function SingleMenuItem({ link }: { link: string }) {
       {({ active }) => (
         <Link
           href={`/${link}`}
-          className={classNames(
+          className={combine(
             active ? "bg-gray-100" : "",
             "block px-4 py-2 link-sm text-gray-700"
           )}
@@ -34,8 +34,6 @@ export function TailwindNavbar() {
   const router = useRouter();
 
   const isActive = (link: string) => {
-    console.log(link);
-
     return router.asPath.startsWith("/" + link);
   };
 
@@ -43,8 +41,8 @@ export function TailwindNavbar() {
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
-          <MobileVersion {...{ isActive, open }} />
           <DesktopVersion {...{ isActive, open }} />
+          <MobileVersion {...{ isActive, open }} />
         </>
       )}
     </Disclosure>
@@ -56,10 +54,10 @@ type NavbarProps = {
   isActive: (link: string) => boolean;
 };
 
-function MobileVersion({ open, isActive }: NavbarProps) {
+function DesktopVersion({ open, isActive }: NavbarProps) {
   return (
-    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 relative flex h-16 items-center justify-between">
-      <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+    <div className="mx-auto max-w-7xl px-2 md :px-6 lg:px-8 relative flex h-16 items-center justify-between">
+      <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
         <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
           <span className="sr-only">Open main menu</span>
           {open ? (
@@ -69,25 +67,25 @@ function MobileVersion({ open, isActive }: NavbarProps) {
           )}
         </Disclosure.Button>
       </div>
-      <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <div className="flex flex-shrink-0 items-center">
+      <div className="flex flex-1 items-stretch justify-end lg:items-center lg:justify-between ">
+        <Link href="/" className="flex flex-shrink-0 items-center">
           <img
             className="h-8 w-auto"
             src="/favicon/apple-touch-icon.png"
             alt="trebeljahr"
           />
-          <span className="text-white">trebeljahr</span>
-        </div>
-        <div className="hidden sm:ml-6 sm:block">
+          <span className="text-white ml-1">trebeljahr</span>
+        </Link>
+        <div className="hidden lg:ml-6 lg:block">
           <div className="flex space-x-4">
             {navigation.map((item) => (
               <Link
                 key={item}
                 href={"/" + item}
-                className={classNames(
+                className={combine(
                   isActive(item)
                     ? "bg-gray-900 text-white hover:text-white cursor-default"
-                    : "text-gray-300 hover:bg-gray-700 hover:text-blue",
+                    : "text-gray-300 hover:bg-gray-700 hover:text-white",
                   "rounded-md px-3 py-2 text-sm font-medium"
                 )}
                 aria-current={isActive(item) ? "page" : undefined}
@@ -95,26 +93,8 @@ function MobileVersion({ open, isActive }: NavbarProps) {
                 {item}
               </Link>
             ))}
-            <Menu as="div" className="relative ml-3">
-              <button className="block text-gray-300 hover:bg-gray-700 hover:text-blue   rounded-md px-3 py-2 text-sm font-medium">
-                resources
-              </button>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <Menu.Items className="absolute left-0 z-10 mt-2 w-48 origin-top-left rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                  {resources.map((item) => (
-                    <SingleMenuItem link={item} />
-                  ))}
-                </Menu.Items>
-              </Transition>
-            </Menu>
+            <DesktopMenu links={resources} text="resources" />
+            <DesktopMenu links={about} text="about" />
           </div>
         </div>
       </div>
@@ -122,15 +102,15 @@ function MobileVersion({ open, isActive }: NavbarProps) {
   );
 }
 
-function DesktopVersion({ isActive }: NavbarProps) {
+function MobileVersion({ isActive }: NavbarProps) {
   return (
-    <Disclosure.Panel className="sm:hidden">
+    <Disclosure.Panel className="lg:hidden">
       <div className="space-y-1 px-2 pb-3 pt-2">
         {navigation.map((item) => (
           <Link
             key={item}
             href={item}
-            className={classNames(
+            className={combine(
               isActive(item)
                 ? "bg-gray-900 text-white"
                 : "text-gray-300 hover:bg-gray-700 hover:text-white",
@@ -142,29 +122,66 @@ function DesktopVersion({ isActive }: NavbarProps) {
           </Link>
         ))}
 
-        <Disclosure.Button>
-          <Menu as="div" className="relative ml-3">
-            <Menu.Button className="block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-base font-medium">
-              <span>resources</span>
-            </Menu.Button>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                {resources.map((item) => (
-                  <SingleMenuItem link={item} />
-                ))}
-              </Menu.Items>
-            </Transition>
-          </Menu>
-        </Disclosure.Button>
+        <MobileMenu links={resources} text="resources" />
+        <MobileMenu links={about} text="about" />
       </div>
     </Disclosure.Panel>
+  );
+}
+
+type MenuProps = {
+  links: string[];
+  text: string;
+};
+
+function DesktopMenu({ links, text }: MenuProps) {
+  return (
+    <Menu as="div" className="block relative ml-3">
+      <Menu.Button className="block text-gray-300 hover:bg-gray-700 hover:text-white   rounded-md px-3 py-2 text-sm font-medium">
+        {text}
+      </Menu.Button>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-20 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          {links.map((item) => (
+            <SingleMenuItem key={item} link={item} />
+          ))}
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+}
+
+function MobileMenu({ links, text }: MenuProps) {
+  return (
+    <Menu as="div" className="relative ml-3">
+      <Disclosure.Button as="div">
+        <Menu.Button className="block text-gray-300 hover:bg-gray-700 hover:text-white rounded-md py-2 text-base font-medium">
+          {text}
+        </Menu.Button>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute left-0 z-20 mt-2 w-48 origin-top-left rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            {links.map((item) => (
+              <SingleMenuItem key={item} link={item} />
+            ))}
+          </Menu.Items>
+        </Transition>
+      </Disclosure.Button>
+    </Menu>
   );
 }
