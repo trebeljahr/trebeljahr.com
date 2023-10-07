e22b2a98783f374fd16edd972e0352cdf0be8929
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2023-10-07 14:54:22 +0200

    fix: Encode URI components for messed up filenames

--- src/scripts/aws/warmupS3Cache.ts
+++ src/scripts/aws/warmupS3Cache.ts
@@ -4,16 +4,14 @@ import { mapToImageProps, nextImageUrl } from "src/lib/mapToImageProps";
 import axios from "axios";
 
 const imageKeys = await getAllStorageObjectKeys(process.env.AWS_BUCKET_NAME!);
-
 let totalInvocations = 0;
 
-const imageSizes = [3840];
+const imageSizes = [128, 256, 384, 640, 750, 828, 1080, 1200, 1920, 2048, 3840];
 for (const imageKey of imageKeys) {
   for (const size of imageSizes) {
     totalInvocations++;
     const url = nextImageUrl("/" + imageKey, size);
-    console.log(url);
-    // axios.get(url);
+    axios.get(url);
   }
 }
 
