7bfa2e679c2a6605585cc9709ba3b5826af203f1
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2024-03-22 16:17:10 +0100

    better field support for graymatter script

--- src/scripts/addGraymatterInformation.ts
+++ src/scripts/addGraymatterInformation.ts
@@ -1,35 +1,24 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import glob from "glob";
-
-// Define the directory and the fields
+import * as glob from "glob";
+import { Note } from "@contentlayer/generated";
+import { execSync } from "child_process";
 
 const directory = "src/content/Notes";
 
 const fields = {
   title: "Enter title",
-  date: "Select date",
-  date_published: "Select published date",
-  date_updated: "Select updated date",
   subtitle: "Enter subtitle",
-  tags: "Enter tags",
   excerpt: "Enter excerpt",
-  coverImage: "Enter cover image URL",
-  author: {
-    name: "Enter author name",
-    image: {
-      src: "Enter image source",
-      alt: "Enter image alt text",
-      width: 0,
-      height: 0,
-    },
-  },
-  ogImage: {
-    src: "Enter Open Graph image source",
-    alt: "Enter Open Graph image alt text",
-    width: 0,
-    height: 0,
+  dateUpdated: new Date().toISOString().slice(0, 10),
+  dateCreated: new Date().toISOString().slice(0, 10),
+  tags: [],
+  cover: {
+    src: "Enter Cover image source",
+    alt: "Enter Cover image alt text",
+    width: 500,
+    height: 500,
   },
   draft: false,
 };
@@ -37,16 +26,65 @@ const fields = {
 const mdFiles = glob.sync(path.join(directory, "**/*.md"));
 console.log(mdFiles);
 
+function getUpdatedDate(filePath: string): string {
+  const directory = path.dirname(filePath);
+  const filename = path.basename(filePath);
+  const command = `git log --format=%aD -1 -- "${filename}"`;
+  const result = execSync(command, { cwd: directory }).toString().trim();
+  return result;
+}
+
+function getCreationDate(filePath: string): string {
+  const directory = path.dirname(filePath);
+  const filename = path.basename(filePath);
+
+  const command = `git log --diff-filter=A --follow --format=%aD -1 -- "${filename}"`;
+  const result = execSync(command, { cwd: directory }).toString().trim();
+  // console.log(filename);
+  // console.log(result);
+  // console.log("-----");
+  return result;
+}
+
 mdFiles.forEach((filePath: string) => {
   const fileContent = fs.readFileSync(filePath, "utf8");
 
+  // console.log(creationDate);
+
+  // Extract the title from the filename
+  const fileName = path.basename(filePath, ".md");
+
   // Parse the file content and check if front matter already exists
-  const { data: frontmatter, content } = matter(fileContent);
+  const { data: frontmatter, content } = matter(fileContent) as unknown as {
+    data: Note;
+    content: string;
+  };
+
+  const dateCreated =
+    frontmatter.date || frontmatter.date_published || getCreationDate(filePath);
+  const dateUpdated = frontmatter.date_updated || getUpdatedDate(filePath);
+
   // If no front matter exists, add the new fields
-  const newFrontmatter = { ...fields, ...frontmatter };
+  const newFrontmatter = {
+    ...fields,
+    ...frontmatter,
+    title: fileName,
+    dateCreated,
+    dateUpdated,
+  };
+
+  delete newFrontmatter["coverImage"];
+  delete newFrontmatter["ogImage"];
+  delete newFrontmatter["author"];
+  delete newFrontmatter["date_published"];
+  delete newFrontmatter["date_updated"];
+  delete newFrontmatter["date"];
+
   const newContent = matter.stringify(content, newFrontmatter);
 
-  console.log(newContent);
+  console.log(newFrontmatter);
+  console.log("-----");
+
   // Write the updated content back to the file
   // fs.writeFileSync(filePath, newContent, "utf8");
 });
