1555788d7a891dc7710997b907538724d38c439c
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2024-03-22 15:39:25 +0100

    add draft for script to add md to all notes

---
+++ src/scripts/add-graymatter-information.ts
@@ -0,0 +1,51 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import glob from "glob";
+
+// Define the directory and the fields
+
+const directory = "src/content/Notes";
+
+const fields = {
+  title: "Enter title",
+  date: "Select date",
+  date_published: "Select published date",
+  date_updated: "Select updated date",
+  subtitle: "Enter subtitle",
+  tags: "Enter tags",
+  excerpt: "Enter excerpt",
+  coverImage: "Enter cover image URL",
+  author: {
+    name: "Enter author name",
+    image: {
+      src: "Enter image source",
+      alt: "Enter image alt text",
+      width: 0,
+      height: 0,
+    },
+  },
+  ogImage: {
+    src: "Enter Open Graph image source",
+    alt: "Enter Open Graph image alt text",
+    width: 0,
+    height: 0,
+  },
+  draft: false,
+};
+
+const mdFiles = glob.sync(path.join(directory, "**/*.md"));
+
+mdFiles.forEach((filePath: string) => {
+  const fileContent = fs.readFileSync(filePath, "utf8");
+
+  // Parse the file content and check if front matter already exists
+  const { data, content } = matter(fileContent);
+  // If no front matter exists, add the new fields
+  const newData = { ...data, ...fields };
+  const newContent = matter.stringify(content, newData);
+
+  console.log(newContent);
+  // Write the updated content back to the file
+  // fs.writeFileSync(filePath, newContent, "utf8");
+});
