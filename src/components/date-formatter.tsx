16200ef756ac7f6e1585b5cd32b7147629ab4914
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2024-03-23 14:43:00 +0100

    update date formatter

--- src/components/date-formatter.tsx
+++ src/components/date-formatter.tsx
@@ -1,11 +1,16 @@
-import { parseISO, format } from "date-fns";
+import { parseISO, format, parse } from "date-fns";
 
 type Props = {
   date: string;
 };
 
 const DateFormatter = ({ date: dateString }: Props) => {
-  const date = parseISO(dateString);
+  let date;
+  if (dateString.includes(".")) {
+    date = parse(dateString, "dd.MM.yyyy", new Date());
+  } else {
+    date = parseISO(dateString);
+  }
   return (
     <div className="publish-date">
       <time dateTime={dateString}>{format(date, "LLLL	d, yyyy")}</time>
