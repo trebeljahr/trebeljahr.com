ff5786bae65ac6c61234f6b409b5c7b8bde7d2d8
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2024-04-08 12:54:43 +0200

    fix image urls for open graph

--- src/components/OpenGraph.tsx
+++ src/components/OpenGraph.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { nextImageUrl } from "src/lib/mapToImageProps";
 interface OpenGraphProps {
   title: string;
   description: string;
@@ -21,7 +22,10 @@ export const OpenGraph: React.FC<OpenGraphProps> = ({
   const baseUrl = "https://trebeljahr.com";
   const imageBaseUrl = `https://${process.env.NEXT_PUBLIC_CLOUDFRONT_ID}.cloudfront.net`;
   const url = new URL(providedLinkUrl, baseUrl).toString();
-  const imageUrl = new URL(providedImageUrl, imageBaseUrl).toString();
+  const imageUrl = nextImageUrl(
+    new URL(providedImageUrl, imageBaseUrl).toString(),
+    1080
+  );
 
   return (
     <Head>
