daa1419657e9032c730a78c5f1ed2aa5f6b22c15
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2024-10-03 14:35:50 +0200

    add nutshell

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 
 interface Props {
   description: string;
@@ -7,10 +8,13 @@ interface Props {
 
 const Meta = ({ description, title }: Props) => {
   return (
-    <Head>
-      <title>{`${title} | trebeljahr.com`}</title>
-      <meta name="description" content={description} />
-    </Head>
+    <>
+      <Head>
+        <title>{`${title} | trebeljahr.com`}</title>
+        <meta name="description" content={description} />
+      </Head>
+      <Script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js"></Script>
+    </>
   );
 };
 
