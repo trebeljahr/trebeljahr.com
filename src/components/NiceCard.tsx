d147b9e83c13f43cf7902338e26e49892342c876
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2024-10-04 07:31:19 +0200

    play with font-pairings

--- src/components/NiceCard.tsx
+++ src/components/NiceCard.tsx
@@ -44,7 +44,7 @@ export const NiceCard = ({
           />
         </div>
         <div className="m-5">
-          <h2 className="pt-0">{title}</h2>
+          <h2 className="pt-0 font-bold leading-snug mb-5">{title}</h2>
           <p className="text-grey">{excerpt}</p>
         </div>
       </div>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,13 +6,16 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* google fonts */}
+          <link rel="preconnect" href="https://rsms.me/" />
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300&family=Libre+Baskerville&family=Merriweather&family=Fira+Code:wght@300&family=Spartan:wght@600&family=Montserrat:wght@500&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,200..800&display=swap"
             rel="stylesheet"
           />
+
           <link
             href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css"
             rel="stylesheet"
