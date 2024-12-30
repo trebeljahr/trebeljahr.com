ff293847ad7aa82e5f6968a9985e96bc8666db0c
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2024-12-30 18:27:38 -0500

    update sitemap config for robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,17 +3,9 @@ const nextSitemapConfig = {
   generateRobotsTxt: true,
   sitemapSize: 7000,
   robotsTxtOptions: {
-    policies: [
-      { userAgent: "*", disallow: "/newsletter/*" },
-      { userAgent: "*", allow: "/" },
-    ],
+    policies: [{ userAgent: "*", allow: "/" }],
   },
-  exclude: [
-    "/newsletter/*",
-    "email-signup-success",
-    "email-signup-error",
-    "emergency",
-  ],
+  exclude: ["email-signup-success", "email-signup-error", "emergency"],
 };
 
 export default nextSitemapConfig;
