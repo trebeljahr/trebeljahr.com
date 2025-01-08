2c2359d42ff70592bb002346c4662aad5721d464
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2025-01-08 10:19:32 -0500

    fix image rendering

--- src/components/Galleries/useCustomLightbox.tsx
+++ src/components/Galleries/useCustomLightbox.tsx
@@ -41,7 +41,7 @@ export const CustomLightBox = ({
       }}
       carousel={{ finite: true }}
       plugins={[Thumbnails, Zoom]}
-      render={{ thumbnail: NextJsSlideImage }}
+      render={{ slide: NextJsSlideImage, thumbnail: NextJsSlideImage }}
       thumbnails={{
         position: "bottom",
         border: 0,
