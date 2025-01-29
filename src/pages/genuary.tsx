907eedb6624099a7025840a787c6fda5afa2cea6
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2025-01-29 05:21:12 -0500

    add more genuary content

--- src/pages/genuary.tsx
+++ src/pages/genuary.tsx
@@ -1,8 +1,22 @@
 import Layout from "@components/Layout";
 import SimpleReactCanvasComponent from "@components/SimpleReactCanvasComponent";
-import { Instance, Instances, OrbitControls, Stage } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { HTMLAttributes, lazy, useEffect, useMemo, useState } from "react";
+import {
+  Instance,
+  Instances,
+  OrbitControls,
+  Stage,
+  useCamera,
+} from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import {
+  HTMLAttributes,
+  lazy,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { Group } from "three";
 
 function random(max: number, min = 0) {
   return min + Math.floor(Math.random() * (max - min + 1));
@@ -114,6 +128,178 @@ function Rule30Sketch() {
   return <SimplerReactCanvasComponent drawFn={drawFn} />;
 }
 
+const palette0 = [
+  "#bc8756",
+  "#24282b",
+  "#836044",
+  "#7ba2a6",
+  "#578392",
+  "#4c6c76",
+  "#335065",
+  "#574231",
+  "#a94c43",
+  "#d6c6a3",
+];
+
+const palette1 = [
+  "#c7522a",
+  "#e5c185",
+  "#f0daa5",
+  "#fbf2c4",
+  "#b8cdab",
+  "#74a892",
+  "#008585",
+  "#004343",
+];
+const palette2 = [
+  "#2156df",
+  "#c9cca8",
+  "#b73c21",
+  "#965fc6",
+  "#d69e58",
+  "#a52f85",
+  "#31c3f4",
+  "#6c1683",
+  "#e76828",
+  "#4c1e6a",
+];
+const palette3 = [
+  "#bc8756",
+  "#24282b",
+  "#836044",
+  "#7ba2a6",
+  "#578392",
+  "#4c6c76",
+  "#335065",
+  "#574231",
+  "#a94c43",
+  "#d6c6a3",
+];
+const palette4 = [
+  "#00202e",
+  "#003f5c",
+  "#2c4875",
+  "#8a508f",
+  "#bc5090",
+  "#ff6361",
+  "#ff8531",
+  "#ffa600",
+  "#ffd380",
+];
+const palette5 = [
+  "#062456",
+  "#1d3867",
+  "#334d77",
+  "#4a6188",
+  "#627799",
+  "#7a8ca9",
+  "#93a2ba",
+  "#adb9cb",
+  "#c8d0dc",
+  "#e3e7ee",
+];
+const palette6 = [
+  "#33322e",
+  "#3d4748",
+  "#475e64",
+  "#4e757e",
+  "#578e9b",
+  "#5da7ba",
+  "#62bed9",
+  "#b2dfeb",
+  "#d7edf3",
+  "#ecf7f8",
+];
+const palette7 = [
+  "#c7522a",
+  "#cb6036",
+  "#cf6e41",
+  "#d68a58",
+  "#dea66f",
+  "#e5c185",
+  "#f0daa5",
+  "#fbf2c4",
+  "#dae0b8",
+  "#b8cdab",
+];
+const palette8 = [
+  "#642915",
+  "#963e20",
+  "#c7522a",
+  "#e5c185",
+  "#fbf2c4",
+  "#74a892",
+  "#008585",
+  "#006464",
+  "#004343",
+];
+const palette9 = [
+  "#03071e",
+  "#211c1b",
+  "#3d3019",
+  "#594417",
+  "#745814",
+  "#906b12",
+  "#ac7f0f",
+  "#c8930d",
+  "#e3a60a",
+  "#ffba08",
+];
+const palette10 = [
+  "#ff7332",
+  "#e96842",
+  "#d25d52",
+  "#bc5262",
+  "#a54771",
+  "#943e79",
+  "#833580",
+  "#722c88",
+  "#60238f",
+];
+const palette11 = [
+  "#f1ddbf",
+  "#cabead",
+  "#a29e9a",
+  "#525e75",
+  "#657980",
+  "#78938a",
+  "#85a78e",
+  "#92ba92",
+  "#aeccae",
+  "#c9ddc9",
+];
+const palette12 = [
+  "#ff4971",
+  "#e44871",
+  "#ca4772",
+  "#af4572",
+  "#954473",
+  "#7a4373",
+  "#604274",
+  "#454174",
+  "#2b3f75",
+  "#103e75",
+];
+
+const createPaletteCycler = (
+  palette: string[],
+  { repeats = 1 }: { repeats?: number } = {}
+) => {
+  let index = 0;
+  let steps = 0;
+
+  return () => {
+    if (steps < repeats) {
+      steps++;
+    } else {
+      index = index + 1 >= palette.length ? 0 : index + 1;
+      steps = 0;
+    }
+
+    return palette[index];
+  };
+};
+
 type DrawFn = (ctx: CanvasRenderingContext2D, cnv: HTMLCanvasElement) => void;
 
 function useDrawCtx(cnv: HTMLCanvasElement | null, drawFn: DrawFn) {
@@ -128,49 +314,72 @@ function useDrawCtx(cnv: HTMLCanvasElement | null, drawFn: DrawFn) {
 }
 
 function MakeSomethingHumanSketch() {
+  //   let rotations = 30;
+  const getNextColor = createPaletteCycler(palette5);
+  const [rotations, setRotations] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRotations((prev) => (prev + 1 >= 60 ? 1 : prev + 1));
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
+  let angle = 360 / rotations;
+  let angleInRadians = (angle * Math.PI) / 180;
+
   const drawFn: DrawFn = (ctx, cnv) => {
     function drawStickFigure(): void {
-      const centerX = cnv.clientWidth / 2;
-      const centerY = cnv.clientHeight / 2;
-
+      ctx.save();
+      ctx.scale(3, 3);
+      ctx.translate(20, 20);
       ctx.lineWidth = 2;
-      ctx.strokeStyle = "#000";
+      ctx.strokeStyle = getNextColor();
 
       const headRadius = 20;
       ctx.beginPath();
-      ctx.arc(centerX, centerY - 60, headRadius, 0, Math.PI * 2);
+      ctx.arc(0, -60, headRadius, 0, Math.PI * 2);
       ctx.stroke();
 
       ctx.beginPath();
-      ctx.moveTo(centerX, centerY - 40);
-      ctx.lineTo(centerX, centerY + 40);
+      ctx.moveTo(0, -40);
+      ctx.lineTo(0, +40);
       ctx.stroke();
 
       ctx.beginPath();
-      ctx.moveTo(centerX, centerY - 20);
-      ctx.lineTo(centerX - 40, centerY);
+      ctx.moveTo(0, -20);
+      ctx.lineTo(-40, 0);
       ctx.stroke();
 
       ctx.beginPath();
-      ctx.moveTo(centerX, centerY - 20);
-      ctx.lineTo(centerX + 40, centerY);
+      ctx.moveTo(0, -20);
+      ctx.lineTo(0 + 40, 0);
       ctx.stroke();
 
       ctx.beginPath();
-      ctx.moveTo(centerX, centerY + 40);
-      ctx.lineTo(centerX - 30, centerY + 80);
+      ctx.moveTo(0, 0 + 40);
+      ctx.lineTo(-30, +80);
       ctx.stroke();
 
       ctx.beginPath();
-      ctx.moveTo(centerX, centerY + 40);
-      ctx.lineTo(centerX + 30, centerY + 80);
+      ctx.moveTo(0, +40);
+      ctx.lineTo(+30, +80);
       ctx.stroke();
+      ctx.restore();
     }
 
-    ctx.fillStyle = "white";
+    ctx.reset();
+    ctx.fillStyle = "#0c1d23";
     ctx.fillRect(0, 0, cnv.width, cnv.height);
+    ctx.translate(cnv.width / 2, cnv.height / 2);
 
-    drawStickFigure();
+    for (let i = 0; i < rotations; i++) {
+      ctx.rotate(angleInRadians);
+      drawStickFigure();
+    }
   };
 
   return <SimplerReactCanvasComponent drawFn={drawFn} />;
@@ -189,11 +398,13 @@ function SimplerReactCanvasComponent({
 function TripleNestedLoopSketch() {
   const size = 60;
 
+  const [index, setIndex] = useState(0);
+
   const boxes = useMemo(() => {
     const out = [];
     const radius = size / 2;
     const center = (size - 1) / 2;
-    const epsilon = 4;
+    const epsilon = 6;
 
     for (let x = 0; x < size * 2; x++) {
       for (let y = 0; y < size * 2; y++) {
@@ -213,13 +424,41 @@ function TripleNestedLoopSketch() {
     return out;
   }, [size]);
 
-  return (
-    <div className="w-[768px] h-[500px]">
-      <Canvas>
-        <OrbitControls />
-        <color attach="background" args={["#FEFCFB"]} />
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1 >= boxes.length ? 0 : prev + 1));
+    }, 1);
 
-        <ambientLight />
+    return () => {
+      clearInterval(interval);
+    };
+  }, [boxes]);
+
+  const groupRef = useRef<Group | null>(null);
+
+  const { camera } = useThree();
+
+  const [increment, setIncrement] = useState(0.5);
+
+  useFrame(() => {
+    if (groupRef.current) {
+      groupRef.current.rotation.y += 0.01;
+    }
+    camera.position.z += increment;
+    if (
+      (camera.position.z >= 60 && increment > 0) ||
+      (camera.position.z <= -10 && increment < 0)
+    ) {
+      setIncrement(-increment);
+    }
+  });
+
+  return (
+    <>
+      {/* <OrbitControls /> */}
+      <color attach="background" args={["#6b6a6a"]} />
+      <ambientLight />
+      <group ref={groupRef}>
         <Stage
           key={size + "stage"}
           adjustCamera
@@ -230,19 +469,19 @@ function TripleNestedLoopSketch() {
           <Instances key={size + "instances"} limit={boxes.length + 1}>
             <boxGeometry args={[1, 1, 1]} />
             <meshStandardMaterial />
-            {boxes.map(({ x, y, z }) => (
+            {boxes.map(({ x, y, z }, i) => (
               <Instance
                 key={`${size}:${x},${y},${z}`}
-                color="#1282A2"
-                scale={2}
+                color={i >= index ? "#a61248" : "#1282A2"}
+                scale={1}
                 position={[x, y + 20, z]}
                 rotation={[0, 0, 0]}
               />
             ))}
           </Instances>
         </Stage>
-      </Canvas>
-    </div>
+      </group>
+    </>
   );
 }
 
@@ -268,7 +507,11 @@ export default function GenuaryPage() {
         <h2>Jan 01. 2021 - Jan 31. 2021</h2>
         <h3>Jan 01. 2021</h3>
         <p>Triple Nested Loop</p>
-        <TripleNestedLoopSketch />
+        <div className="w-[768px] h-[500px]">
+          <Canvas camera={{ fov: 90 }}>
+            <TripleNestedLoopSketch />
+          </Canvas>
+        </div>
 
         <h3>Jan 02. 2021</h3>
         <p>Rule 30</p>
@@ -278,9 +521,9 @@ export default function GenuaryPage() {
         <p>Make something human</p>
         <MakeSomethingHumanSketch />
 
-        <h3>Jan 04. 2021</h3>
+        {/* <h3>Jan 04. 2021</h3>
         <p>Small Areas of Symmetry</p>
-        <SmallAreasOfSymmetrySketch />
+        <SmallAreasOfSymmetrySketch /> */}
       </main>
     </Layout>
   );
