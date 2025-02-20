8921156d26273b15a37cbd8b5722745edaf5e7a5
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2025-02-20 17:17:23 -0500

    add animations

--- src/components/Navbar/TailwindNavbar.tsx
+++ src/components/Navbar/TailwindNavbar.tsx
@@ -14,6 +14,8 @@ import {
 import { DarkModeHandler } from "./DarkModeHandler";
 import { useScrollLock } from "src/hooks/useScrollLock";
 import clsx from "clsx";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 const navigation = ["posts", "newsletters", "photography"];
 const resources = ["quotes", "booknotes", "needlestack", "podcastnotes"];
@@ -132,54 +134,67 @@ function DesktopVersion() {
 }
 
 export const LeftSmallNavbar = () => {
+  const [open, setOpen] = useState(false);
+  const toggle = () => setOpen((prev) => !prev);
+  const close = () => setOpen(false);
+
   return (
-    <Disclosure>
-      {({ open, close }) => (
-        <div className="prose-a:no-underline prose-a:hover:text-inherit prose-a:font-normal font-normal prose-a:text-inherit">
-          <DisclosureButton
-            className={clsx(
-              "fixed top-2 z-50 bg-white dark:bg-gray-900 hover:bg-gray-200 dark:hover:bg-gray-700 flex items-center justify-center rounded-full p-3 ",
-              open ? "left-[175px]" : "left-2"
-            )}
-          >
-            <span className="sr-only">Open main menu</span>
-            {open ? <FiX className="size-4" /> : <FiMenu className="size-4" />}
-          </DisclosureButton>
-          <DisclosurePanel className="absolute z-40 top-0 left-0 py-2 w-[220px] h-screen  bg-white dark:bg-gray-900">
-            <div className="absolute bottom-2 right-2">
-              <DarkModeHandler />
-            </div>
-
-            <div className="flex flex-col pb-3 pt-2 px-3 items-start justify-start">
-              <RicosSiteBanner />
-              <div className="pt-3">
-                {navigation.map((item) => (
-                  <Link
-                    key={item}
-                    href={"/" + item}
-                    className="block w-fit rounded-md px-3 py-2 hover:bg-gray-200 dark:hover:bg-gray-700 "
-                  >
-                    <DisclosureButton>{item}</DisclosureButton>
-                  </Link>
-                ))}
-
-                <CollapsibleMenuMobile
-                  links={resources}
-                  text="resources"
-                  closeNav={close}
-                  left
-                />
-                <CollapsibleMenuMobile
-                  links={about}
-                  text="about"
-                  closeNav={close}
-                  left
-                />
+    <>
+      <div className="prose-a:no-underline prose-a:hover:text-inherit prose-a:font-normal font-normal prose-a:text-inherit">
+        <button
+          className={clsx(
+            "fixed top-2 z-50 bg-white dark:bg-gray-900 hover:bg-gray-200 dark:hover:bg-gray-700 flex items-center justify-center rounded-full p-3 transition-[left] duration-300",
+            open ? "left-[175px]" : "left-2"
+          )}
+          onClick={toggle}
+        >
+          <span className="sr-only">Open main menu</span>
+          {open ? <FiX className="size-4" /> : <FiMenu className="size-4" />}
+        </button>
+        <AnimatePresence>
+          {open && (
+            <motion.div
+              initial={{ x: -220 }}
+              animate={{ x: 0 }}
+              exit={{ x: -220 }}
+              transition={{ duration: 0.3 }}
+              className="absolute z-40 top-0 left-0 py-2 w-[220px] h-screen  bg-white dark:bg-gray-900"
+            >
+              <div className="absolute bottom-2 right-2">
+                <DarkModeHandler />
               </div>
-            </div>
-          </DisclosurePanel>
-        </div>
-      )}
-    </Disclosure>
+
+              <div className="flex flex-col pb-3 pt-2 px-3 items-start justify-start">
+                <RicosSiteBanner />
+                <div className="pt-3">
+                  {navigation.map((item) => (
+                    <Link
+                      key={item}
+                      href={"/" + item}
+                      className="block w-fit rounded-md px-3 py-2 hover:bg-gray-200 dark:hover:bg-gray-700 "
+                    >
+                      {item}
+                    </Link>
+                  ))}
+
+                  <CollapsibleMenuMobile
+                    links={resources}
+                    text="resources"
+                    closeNav={close}
+                    left
+                  />
+                  <CollapsibleMenuMobile
+                    links={about}
+                    text="about"
+                    closeNav={close}
+                    left
+                  />
+                </div>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </>
   );
 };
--- src/pages/r3f/shader-art-demo.tsx
+++ src/pages/r3f/shader-art-demo.tsx
@@ -4,6 +4,7 @@ import { ThreeFiberLayout } from "@components/dom/Layout";
 import Layout from "@components/Layout";
 import { Canvas } from "@react-three/fiber";
 import controllableShaderArt from "@shaders/controllableShaderArt.glsl";
+import { AnimatePresence, motion } from "framer-motion";
 import { Leva, useControls } from "leva";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
@@ -66,96 +67,104 @@ const InfoButton = () => {
       >
         <FaInfo className="size-4" />
       </button>
-      {showInfo && (
-        <div className="absolute bottom-0 left-0 z-10 bg-white dark:bg-gray-900 p-3 px-5 rounded-md">
-          <div className="max-w-prose">
-            <p className="pr-5">
-              This shader art demo allows you to control a fractal shader with
-              sliders. Go create something cool! You can share your current
-              settings with others by clicking the share button in the bottom
-              right. This will copy the URL which you can then send around.
-            </p>
-            <p className="!mb-0">Try some presets!</p>
-            <Link href="/r3f/shader-art-demo?chosenShape=0&chosenPalette=5&repetitions=2.1&speedFactor=0.6&scaleFactor=2.2&space=7.2&depth=4.199999999999999&contrast=1.1&strength=0.0073&rgbStrength=%5B1%2C1%2C1%5D">
-              1
-            </Link>
-            ,{" "}
-            <Link href="/r3f/shader-art-demo?chosenShape=5&chosenPalette=3&repetitions=1.5&speedFactor=0.5&scaleFactor=4&space=8&depth=8&contrast=1&strength=0.003&rgbStrength=%5B1%2C1%2C1%5D">
-              2
-            </Link>
-            ,{" "}
-            <Link href="/r3f/shader-art-demo?chosenShape=5&chosenPalette=7&repetitions=1.5&speedFactor=0.5&scaleFactor=0.6999999999999997&space=15.8&depth=3.5&contrast=2.3&strength=0.004200000000000001&rgbStrength=%5B1%2C1%2C1%5D">
-              3
-            </Link>
-            ,{" "}
-            <Link href="/r3f/shader-art-demo?chosenShape=5&chosenPalette=1&repetitions=3&speedFactor=1.1&scaleFactor=2.7&space=20&depth=0.1999999999999993&contrast=1.2&strength=0.0076&rgbStrength=%5B1%2C1%2C1%5D">
-              4
-            </Link>
-            ,{" "}
-            <Link href="/r3f/shader-art-demo?chosenShape=0&chosenPalette=0&repetitions=4.4&speedFactor=0.3&scaleFactor=2.7&space=20&depth=1.0999999999999996&contrast=1.2&strength=0.0076&rgbStrength=%5B1%2C1%2C1%5D">
-              5
-            </Link>
-            ,{" "}
-            <Link href="/r3f/shader-art-demo?chosenShape=3&chosenPalette=2&repetitions=4.4&speedFactor=0.3&scaleFactor=6.5&space=16.5&depth=1.2999999999999998&contrast=1.1&strength=0.0098&rgbStrength=%5B1%2C1%2C1%5D">
-              6
-            </Link>
-            ,{" "}
-            <Link href="/r3f/shader-art-demo?chosenShape=2&chosenPalette=2&repetitions=9.4&speedFactor=0.4&scaleFactor=7.4&space=1.5&depth=3.8&contrast=0.8&strength=0.0078000000000000005&rgbStrength=%5B1%2C1%2C1%5D">
-              7
-            </Link>
-            ,{" "}
-            <Link href="/r3f/shader-art-demo?chosenShape=6&chosenPalette=2&repetitions=1.5&speedFactor=0.5&scaleFactor=0.3999999999999999&space=16.5&depth=2.5999999999999996&contrast=1.2&strength=0.0088&rgbStrength=%5B1%2C1%2C1%5D">
-              8
-            </Link>
-            ,{" "}
-            <Link href="/r3f/shader-art-demo?chosenShape=6&chosenPalette=2&repetitions=1.8&speedFactor=0.5&scaleFactor=0.3999999999999999&space=5.1&depth=10&contrast=1.2&strength=0.0083&rgbStrength=%5B0.5%2C0.5%2C0.5%5D">
-              9
-            </Link>
-            ,{" "}
-            <Link href="/r3f/shader-art-demo?chosenShape=6&chosenPalette=5&repetitions=2.1&speedFactor=0.6&scaleFactor=10&space=15.4&depth=4.199999999999999&contrast=1&strength=0.0035&rgbStrength=%5B1%2C1%2C1%5D">
-              10
-            </Link>
-            ,{" "}
-            <Link href="/r3f/shader-art-demo?chosenShape=6&chosenPalette=1&repetitions=1.4&speedFactor=0.5&scaleFactor=7.4&space=1.5&depth=8&contrast=0.8&strength=0.001&rgbStrength=%5B1%2C1%2C1%5D">
-              11
-            </Link>
-            ,{" "}
-            <Link href="/r3f/shader-art-demo?chosenShape=2&chosenPalette=3&repetitions=4.4&speedFactor=1.8&scaleFactor=2.7&space=7.1&depth=8&contrast=0.8&strength=0.0023&rgbStrength=%5B1%2C1%2C1%5D">
-              12
-            </Link>
-            <p>
-              This project is largely inspired by the work of{" "}
-              <a href="https://www.kishimisu.art/">kishimisu</a>, specifically
-              his{" "}
-              <a href="https://www.youtube.com/watch?v=f4s1h2YETNY">
-                intro to creative shader art video
-              </a>
-              , make sure to check it out on YT! The demo is using{" "}
-              <a href="https://iquilezles.org/articles/distfunctions2d/">
-                2D SDF functions by Inigo Quilez
-              </a>{" "}
-              as well as{" "}
-              <a href="https://iquilezles.org/articles/palettes/">
-                his palette approach
-              </a>{" "}
-              for coloring!
-            </p>
-            <p>
-              Also, this project would not be possible without the use of{" "}
-              <a href="https://github.com/pmndrs/react-three-fiber">r3f</a>,{" "}
-              <a href="https://github.com/pmndrs/leva">leva</a>, and the whole{" "}
-              <a href="https://pmnd.rs/">pmndrs</a> ecosystem. Thanks for
-              building all these crazy tools! 😊
-            </p>
-          </div>
-          <button
-            className="absolute top-2 right-2 bg-white dark:bg-gray-900 hover:bg-gray-200 dark:hover:bg-gray-700 p-2 rounded-full"
-            onClick={toggleInfo}
+      <AnimatePresence>
+        {showInfo && (
+          <motion.div
+            initial={{ x: -690 }}
+            animate={{ x: 0 }}
+            exit={{ x: -690 }}
+            transition={{ duration: 0.3 }}
+            className="absolute bottom-0 left-0 z-10 bg-white dark:bg-gray-900 p-3 px-5 rounded-md"
           >
-            <FiX className="size-5 " />
-          </button>
-        </div>
-      )}
+            <div className="max-w-prose">
+              <p className="pr-5">
+                This shader art demo allows you to control a fractal shader with
+                sliders. Go create something cool! You can share your current
+                settings with others by clicking the share button in the bottom
+                right. This will copy the URL which you can then send around.
+              </p>
+              <p className="!mb-0">Try some presets!</p>
+              <Link href="/r3f/shader-art-demo?chosenShape=0&chosenPalette=5&repetitions=2.1&speedFactor=0.6&scaleFactor=2.2&space=7.2&depth=4.199999999999999&contrast=1.1&strength=0.0073&rgbStrength=%5B1%2C1%2C1%5D">
+                1
+              </Link>
+              ,{" "}
+              <Link href="/r3f/shader-art-demo?chosenShape=5&chosenPalette=3&repetitions=1.5&speedFactor=0.5&scaleFactor=4&space=8&depth=8&contrast=1&strength=0.003&rgbStrength=%5B1%2C1%2C1%5D">
+                2
+              </Link>
+              ,{" "}
+              <Link href="/r3f/shader-art-demo?chosenShape=5&chosenPalette=7&repetitions=1.5&speedFactor=0.5&scaleFactor=0.6999999999999997&space=15.8&depth=3.5&contrast=2.3&strength=0.004200000000000001&rgbStrength=%5B1%2C1%2C1%5D">
+                3
+              </Link>
+              ,{" "}
+              <Link href="/r3f/shader-art-demo?chosenShape=5&chosenPalette=1&repetitions=3&speedFactor=1.1&scaleFactor=2.7&space=20&depth=0.1999999999999993&contrast=1.2&strength=0.0076&rgbStrength=%5B1%2C1%2C1%5D">
+                4
+              </Link>
+              ,{" "}
+              <Link href="/r3f/shader-art-demo?chosenShape=0&chosenPalette=0&repetitions=4.4&speedFactor=0.3&scaleFactor=2.7&space=20&depth=1.0999999999999996&contrast=1.2&strength=0.0076&rgbStrength=%5B1%2C1%2C1%5D">
+                5
+              </Link>
+              ,{" "}
+              <Link href="/r3f/shader-art-demo?chosenShape=3&chosenPalette=2&repetitions=4.4&speedFactor=0.3&scaleFactor=6.5&space=16.5&depth=1.2999999999999998&contrast=1.1&strength=0.0098&rgbStrength=%5B1%2C1%2C1%5D">
+                6
+              </Link>
+              ,{" "}
+              <Link href="/r3f/shader-art-demo?chosenShape=2&chosenPalette=2&repetitions=9.4&speedFactor=0.4&scaleFactor=7.4&space=1.5&depth=3.8&contrast=0.8&strength=0.0078000000000000005&rgbStrength=%5B1%2C1%2C1%5D">
+                7
+              </Link>
+              ,{" "}
+              <Link href="/r3f/shader-art-demo?chosenShape=6&chosenPalette=2&repetitions=1.5&speedFactor=0.5&scaleFactor=0.3999999999999999&space=16.5&depth=2.5999999999999996&contrast=1.2&strength=0.0088&rgbStrength=%5B1%2C1%2C1%5D">
+                8
+              </Link>
+              ,{" "}
+              <Link href="/r3f/shader-art-demo?chosenShape=6&chosenPalette=2&repetitions=1.8&speedFactor=0.5&scaleFactor=0.3999999999999999&space=5.1&depth=10&contrast=1.2&strength=0.0083&rgbStrength=%5B0.5%2C0.5%2C0.5%5D">
+                9
+              </Link>
+              ,{" "}
+              <Link href="/r3f/shader-art-demo?chosenShape=6&chosenPalette=5&repetitions=2.1&speedFactor=0.6&scaleFactor=10&space=15.4&depth=4.199999999999999&contrast=1&strength=0.0035&rgbStrength=%5B1%2C1%2C1%5D">
+                10
+              </Link>
+              ,{" "}
+              <Link href="/r3f/shader-art-demo?chosenShape=6&chosenPalette=1&repetitions=1.4&speedFactor=0.5&scaleFactor=7.4&space=1.5&depth=8&contrast=0.8&strength=0.001&rgbStrength=%5B1%2C1%2C1%5D">
+                11
+              </Link>
+              ,{" "}
+              <Link href="/r3f/shader-art-demo?chosenShape=2&chosenPalette=3&repetitions=4.4&speedFactor=1.8&scaleFactor=2.7&space=7.1&depth=8&contrast=0.8&strength=0.0023&rgbStrength=%5B1%2C1%2C1%5D">
+                12
+              </Link>
+              <p>
+                This project is largely inspired by the work of{" "}
+                <a href="https://www.kishimisu.art/">kishimisu</a>, specifically
+                his{" "}
+                <a href="https://www.youtube.com/watch?v=f4s1h2YETNY">
+                  intro to creative shader art video
+                </a>
+                , make sure to check it out on YT! The demo is using{" "}
+                <a href="https://iquilezles.org/articles/distfunctions2d/">
+                  2D SDF functions by Inigo Quilez
+                </a>{" "}
+                as well as{" "}
+                <a href="https://iquilezles.org/articles/palettes/">
+                  his palette approach
+                </a>{" "}
+                for coloring!
+              </p>
+              <p>
+                Also, this project would not be possible without the use of{" "}
+                <a href="https://github.com/pmndrs/react-three-fiber">r3f</a>,{" "}
+                <a href="https://github.com/pmndrs/leva">leva</a>, and the whole{" "}
+                <a href="https://pmnd.rs/">pmndrs</a> ecosystem. Thanks for
+                building all these crazy tools! 😊
+              </p>
+            </div>
+            <button
+              className="absolute top-2 right-2 bg-white dark:bg-gray-900 hover:bg-gray-200 dark:hover:bg-gray-700 p-2 rounded-full"
+              onClick={toggleInfo}
+            >
+              <FiX className="size-5 " />
+            </button>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
